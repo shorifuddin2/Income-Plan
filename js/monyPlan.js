8e15f9c92e460677fd71b7ba3db60e42f3ce0275
shorifuddin2 <shorifuddin240915@gmail.com>
2022-02-17 06:10:00 +0600

    desply item complyte

--- js/monyPlan.js
+++ js/monyPlan.js
@@ -1,2 +1,39 @@
+const calculateButton = document.getElementById('calculate-btn');
+calculateButton.addEventListener('click',function(){
+    const userInput = document.getElementById('total-income');
+    document.getElementById('balance').innerText = userInput.value;
+    userInput.value='';
 
-console.log('hello javascript');
+
+    function updateBalance(amount, isAdd){
+        const balanceTotal = document.getElementById('total-coroch');
+        const balanceTotalText = balanceTotal.innerText;
+        const previousBalanceTotal = parseFloat(balanceTotalText);
+        if(isAdd == true){
+           balanceTotal.innerText = previousBalanceTotal + amount;
+        }
+        // else{
+        //    balanceTotal.innerText = previousBalanceTotal - amount;
+        // }
+     }
+    
+
+})
+
+const depositAmount = getInputValue('deposit-input');
+   if(depositAmount > 0){
+      updateTotalField('deposit-total',depositAmount);
+      updateBalance(depositAmount, true);
+   }
+
+// let calculateButton =document.getElementById('calculate-btn');
+// calculateButton.addEventListener('click',function(){
+
+    // const foodInput = document.getElementById('food-input').innerText;
+    // const machVaraInput = document.getElementById('mach-vara').innerText;
+    // const extraInput = document.getElementById('extra-coroch').innerText;
+    // const corochTotal = parseFloat(foodInput) + parseFloat(machVaraInput) + parseFloat(extraInput);
+    
+    // const totalCoroch = document.getElementById('total-coroch');
+    // totalCoroch.innerText = corochTotal;
+// })
